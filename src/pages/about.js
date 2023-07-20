46f6bcc47b8566c7fda9e3ba60abe1e19b74843e
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-07-20 21:53:21 +0200

    Grid mosaic style images container added
    I have added grid mosaic style container for my images in about section
    of my website.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,9 @@
+import gridImage1 from "../assets/grid-image-1.jpg";
+import gridImage2 from "../assets/grid-image-2.jpg";
+import gridImage3 from "../assets/grid-image-3.jpg";
+import gridImage4 from "../assets/grid-image-4.jpg";
+import gridImage5 from "../assets/grid-image-5.jpg";
+
 function createWebsiteColumns() {
   const aboutPageSection = document.createElement("section");
   aboutPageSection.id = "about-container";
@@ -46,12 +52,64 @@ function createParaInformation() {
   return aboutPageSection;
 }
 
+function createGridImages() {
+  const aboutPageSection = document.querySelector("#about-container");
+  const secondColumn = document.querySelector(".second-column");
+
+  const gridImagesContainer = document.createElement("div");
+  gridImagesContainer.className = "grid-images-container";
+  secondColumn.appendChild(gridImagesContainer);
+
+  const ulGridImages = document.createElement("ul");
+
+  const li1 = document.createElement("li");
+  li1.className = "image-1";
+  const li2 = document.createElement("li");
+  li2.className = "image-2";
+  const li3 = document.createElement("li");
+  li3.className = "image-3";
+  const li4 = document.createElement("li");
+  li4.className = "image-4";
+  const li5 = document.createElement("li");
+  li5.className = "image-5";
+
+  const imageOne = document.createElement("img");
+  imageOne.src = gridImage1;
+  const imageTwo = document.createElement("img");
+  imageTwo.src = gridImage2;
+  const imageThree = document.createElement("img");
+  imageThree.src = gridImage3;
+  const imageFour = document.createElement("img");
+  imageFour.src = gridImage4;
+  const imageFive = document.createElement("img");
+  imageFive.src = gridImage5;
+
+  gridImagesContainer.appendChild(ulGridImages);
+  ulGridImages.appendChild(li1);
+  li1.appendChild(imageFour);
+
+  ulGridImages.appendChild(li2);
+  li2.appendChild(imageTwo);
+
+  ulGridImages.appendChild(li3);
+  li3.appendChild(imageThree);
+
+  ulGridImages.appendChild(li4);
+  li4.appendChild(imageOne);
+
+  ulGridImages.appendChild(li5);
+  li5.appendChild(imageFive);
+
+  return aboutPageSection;
+}
+
 function loadAbout() {
   createWebsiteColumns();
   const contentContainer = document.querySelector("#content");
   contentContainer.appendChild(createWebsiteColumns());
   contentContainer.appendChild(createContentAbout());
   contentContainer.appendChild(createParaInformation());
+  contentContainer.appendChild(createGridImages());
 }
 
 export default loadAbout;
