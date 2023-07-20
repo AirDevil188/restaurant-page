abaa6095d7cd5075421c168d463e552612c9cf31
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-07-20 18:28:53 +0200

    Add paragraph in about section of the page
    I have placed the first paragraph in about section.

--- src/pages/about.js
+++ src/pages/about.js
@@ -13,12 +13,35 @@ function createWebsiteColumns() {
   return aboutPageSection;
 }
 
-function createAboutPage() {
+function createContentAbout() {
   const aboutPageSection = document.querySelector("#about-container");
   const secondColumn = document.querySelector(".second-column");
+
   const h2About = document.createElement("h2");
   h2About.textContent = "About Us";
   secondColumn.appendChild(h2About);
+
+  const aboutUsIcon = document.createElement("span");
+  aboutUsIcon.className = "material-symbols-outlined";
+  aboutUsIcon.textContent = "history_edu";
+  secondColumn.appendChild(aboutUsIcon);
+  const contentSeparator = document.createElement("div");
+  contentSeparator.className = "separator";
+  secondColumn.appendChild(contentSeparator);
+
+  return aboutPageSection;
+}
+
+function createParaInformation() {
+  const aboutPageSection = document.querySelector("#about-container");
+  const secondColumn = document.querySelector(".second-column");
+
+  const aboutUsContainer = document.createElement("div");
+  aboutUsContainer.className = "about-us-container";
+  secondColumn.appendChild(aboutUsContainer);
+  const paraAboutUs = document.createElement("p");
+  aboutUsContainer.appendChild(paraAboutUs);
+  paraAboutUs.textContent = "Since  1st of January 2000. the quality was always our imperative. We started as a small pub in a relative small town in Serbia, called Apatin. What started as a family job, turned out to be something more then that. Our vision is that every costumer walks out happy! And after more then twenty years of experience we are still looking to innovate and learn new things.";
 
   return aboutPageSection;
 }
@@ -27,7 +50,8 @@ function loadAbout() {
   createWebsiteColumns();
   const contentContainer = document.querySelector("#content");
   contentContainer.appendChild(createWebsiteColumns());
-  contentContainer.appendChild(createAboutPage());
+  contentContainer.appendChild(createContentAbout());
+  contentContainer.appendChild(createParaInformation());
 }
 
 export default loadAbout;
