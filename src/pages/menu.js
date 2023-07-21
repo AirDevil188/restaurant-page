caa2ce57560b3b699854c659d58d07b6f48f7b09
AirDevil188 <urosculibrk@ymail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --global pull.rebase false[200~git config --get user.namegit config --get user.namegit config --get user.emailcleargit config --global user.email urosculibrk@ymail.com>
2023-07-21 22:38:35 +0200

    Starting work on the menu page
    I have started to code my menu section of the page.

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,18 +1,53 @@
+function createWebsiteColumns() {
+  const menuPageSection = document.createElement("section");
+  menuPageSection.id = "menu";
+
+  const firstColumn = document.createElement("div");
+  menuPageSection.appendChild(firstColumn);
+  const secondColumn = document.createElement("div");
+  menuPageSection.appendChild(secondColumn);
+  secondColumn.className = "second-column";
+  const thirdColum = document.createElement("div");
+  menuPageSection.appendChild(thirdColum);
+
+  return menuPageSection;
+}
+
+function createMenuContent() {
+  const menuPageSection = document.querySelector("#menu");
+  const secondColumn = document.querySelector(".second-column");
+
+  const menuH2 = document.createElement("h2");
+  menuH2.textContent = "MENU";
+  const menuIcon = document.createElement("span");
+  menuIcon.className = "material-symbols-outlined";
+  menuIcon.textContent = "restaurant_menu";
+  const separator = document.createElement("div");
+  separator.className = "separator";
+
+  secondColumn.appendChild(menuH2);
+  secondColumn.appendChild(menuIcon);
+  secondColumn.appendChild(separator);
+
+  return menuPageSection;
+}
+
 function createMenu() {
+  const menuPageSection = document.querySelector("#menu");
+  const secondColumn = document.querySelector(".second-column");
+
   const menuContainer = document.createElement("div");
+  menuContainer.className = "menu-container";
+  secondColumn.appendChild(menuContainer);
+  menuContainer.textContent = "test";
 
-  menuContainer.classList.add("menu-container");
-
-  const testH1Title = document.createElement("h1");
-  testH1Title.textContent = "Test for my website!";
-
-  menuContainer.appendChild(testH1Title);
-
-  return menuContainer;
+  return menuPageSection;
 }
 
 function loadMenu() {
   const contentContainer = document.querySelector("#content");
+  contentContainer.appendChild(createWebsiteColumns());
+  contentContainer.appendChild(createMenuContent());
   contentContainer.appendChild(createMenu());
 }
 
